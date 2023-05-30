7a44759fd384038e44baa40ef7b53cbd3c736510
hospotho <a155268747@gmail.com>
2023-05-30 14:27:53 +0800

    rebase test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Testing By Kelvin";
+            this.label1.Text = "Testing By Kelvin  ## 30/5 by tony";
             // 
             // Form1
             // 
