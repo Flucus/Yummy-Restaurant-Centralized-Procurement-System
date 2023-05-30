3bbc003ef7891b61d41dd561df4012113530f82c
Ka Wing CHAN <220014654@stu.vtc.edu.hk>
2023-05-30 14:15:16 +0800

    testing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,18 +34,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(214, 102);
+            this.label1.Location = new System.Drawing.Point(214, 94);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.Size = new System.Drawing.Size(90, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "test pull request from Flucus\'s PC";
+            this.label1.Text = "Testing By Kelvin";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 390);
+            this.ClientSize = new System.Drawing.Size(600, 360);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "Form1";
