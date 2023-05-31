13864c3a3b77491c9350ab7c4cfdebb08ee2c2ac
Flucus ~ NightSky <fondbuckle@protonmail.com>
2023-05-31 21:19:52 +0800

    -added Stock Management Form

--- FrmAdmin.Designer.cs
+++ FrmAdmin.Designer.cs
@@ -38,15 +38,16 @@
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnCreate = new System.Windows.Forms.Button();
             this.btnLog = new System.Windows.Forms.Button();
+            this.lblAdminPage = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvUserList)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvUserList
             // 
             this.dgvUserList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvUserList.Location = new System.Drawing.Point(12, 12);
+            this.dgvUserList.Location = new System.Drawing.Point(12, 37);
             this.dgvUserList.Name = "dgvUserList";
-            this.dgvUserList.Size = new System.Drawing.Size(339, 349);
+            this.dgvUserList.Size = new System.Drawing.Size(339, 324);
             this.dgvUserList.TabIndex = 0;
             // 
             // btnSearch
@@ -55,7 +56,7 @@
             this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSearch.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSearch.ForeColor = System.Drawing.Color.Black;
-            this.btnSearch.Location = new System.Drawing.Point(357, 370);
+            this.btnSearch.Location = new System.Drawing.Point(375, 370);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(134, 38);
             this.btnSearch.TabIndex = 6;
@@ -105,7 +106,7 @@
             this.btnModify.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnModify.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnModify.ForeColor = System.Drawing.Color.Black;
-            this.btnModify.Location = new System.Drawing.Point(357, 323);
+            this.btnModify.Location = new System.Drawing.Point(375, 323);
             this.btnModify.Name = "btnModify";
             this.btnModify.Size = new System.Drawing.Size(134, 38);
             this.btnModify.TabIndex = 11;
@@ -118,7 +119,7 @@
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDelete.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDelete.ForeColor = System.Drawing.Color.Black;
-            this.btnDelete.Location = new System.Drawing.Point(357, 279);
+            this.btnDelete.Location = new System.Drawing.Point(375, 279);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(134, 38);
             this.btnDelete.TabIndex = 12;
@@ -131,7 +132,7 @@
             this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCreate.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCreate.ForeColor = System.Drawing.Color.Black;
-            this.btnCreate.Location = new System.Drawing.Point(357, 235);
+            this.btnCreate.Location = new System.Drawing.Point(375, 235);
             this.btnCreate.Name = "btnCreate";
             this.btnCreate.Size = new System.Drawing.Size(134, 38);
             this.btnCreate.TabIndex = 13;
@@ -144,18 +145,30 @@
             this.btnLog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLog.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLog.ForeColor = System.Drawing.Color.Black;
-            this.btnLog.Location = new System.Drawing.Point(357, 191);
+            this.btnLog.Location = new System.Drawing.Point(375, 191);
             this.btnLog.Name = "btnLog";
             this.btnLog.Size = new System.Drawing.Size(134, 38);
             this.btnLog.TabIndex = 14;
             this.btnLog.Text = "Activity Log";
             this.btnLog.UseVisualStyleBackColor = false;
             // 
+            // lblAdminPage
+            // 
+            this.lblAdminPage.AutoSize = true;
+            this.lblAdminPage.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdminPage.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblAdminPage.Location = new System.Drawing.Point(7, 9);
+            this.lblAdminPage.Name = "lblAdminPage";
+            this.lblAdminPage.Size = new System.Drawing.Size(512, 25);
+            this.lblAdminPage.TabIndex = 15;
+            this.lblAdminPage.Text = "The Yummy Restaurant System Admin Page";
+            // 
             // FrmAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 420);
+            this.ClientSize = new System.Drawing.Size(521, 420);
+            this.Controls.Add(this.lblAdminPage);
             this.Controls.Add(this.btnLog);
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.btnDelete);
@@ -186,5 +199,6 @@
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Label lblAdminPage;
     }
 }
