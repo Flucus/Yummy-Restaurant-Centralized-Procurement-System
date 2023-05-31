a1cbf1c2585673f4c8f49c774354609d978d796d
Flucus ~ NightSky <fondbuckle@protonmail.com>
2023-05-31 19:10:10 +0800

    - update Login Form - add Menu Form

---
+++ FrmMenu.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace YummyRestaurantSystem
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHelloMsg = new System.Windows.Forms.Label();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnAccount = new System.Windows.Forms.Button();
+            this.btnRestaurant = new System.Windows.Forms.Button();
+            this.btnPurchase = new System.Windows.Forms.Button();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnWarehouse = new System.Windows.Forms.Button();
+            this.btnCategory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHelloMsg
+            // 
+            this.lblHelloMsg.AutoSize = true;
+            this.lblHelloMsg.Font = new System.Drawing.Font("Stencil", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHelloMsg.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblHelloMsg.Location = new System.Drawing.Point(12, 13);
+            this.lblHelloMsg.Name = "lblHelloMsg";
+            this.lblHelloMsg.Size = new System.Drawing.Size(164, 25);
+            this.lblHelloMsg.TabIndex = 3;
+            this.lblHelloMsg.Text = "Hello XXXXX!";
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLogout.Location = new System.Drawing.Point(241, 12);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(126, 34);
+            this.btnLogout.TabIndex = 7;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // btnAccount
+            // 
+            this.btnAccount.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAccount.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccount.ForeColor = System.Drawing.Color.Black;
+            this.btnAccount.Location = new System.Drawing.Point(50, 59);
+            this.btnAccount.Name = "btnAccount";
+            this.btnAccount.Size = new System.Drawing.Size(134, 38);
+            this.btnAccount.TabIndex = 8;
+            this.btnAccount.Text = "Account";
+            this.btnAccount.UseVisualStyleBackColor = false;
+            // 
+            // btnRestaurant
+            // 
+            this.btnRestaurant.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRestaurant.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestaurant.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestaurant.ForeColor = System.Drawing.Color.Black;
+            this.btnRestaurant.Location = new System.Drawing.Point(202, 59);
+            this.btnRestaurant.Name = "btnRestaurant";
+            this.btnRestaurant.Size = new System.Drawing.Size(134, 38);
+            this.btnRestaurant.TabIndex = 9;
+            this.btnRestaurant.Text = "Restaurant";
+            this.btnRestaurant.UseVisualStyleBackColor = false;
+            // 
+            // btnPurchase
+            // 
+            this.btnPurchase.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPurchase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPurchase.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPurchase.ForeColor = System.Drawing.Color.Black;
+            this.btnPurchase.Location = new System.Drawing.Point(50, 120);
+            this.btnPurchase.Name = "btnPurchase";
+            this.btnPurchase.Size = new System.Drawing.Size(134, 38);
+            this.btnPurchase.TabIndex = 10;
+            this.btnPurchase.Text = "Purchase";
+            this.btnPurchase.UseVisualStyleBackColor = false;
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdmin.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdmin.ForeColor = System.Drawing.Color.Black;
+            this.btnAdmin.Location = new System.Drawing.Point(202, 120);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(134, 38);
+            this.btnAdmin.TabIndex = 11;
+            this.btnAdmin.Text = "Admin";
+            this.btnAdmin.UseVisualStyleBackColor = false;
+            // 
+            // btnWarehouse
+            // 
+            this.btnWarehouse.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnWarehouse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWarehouse.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWarehouse.ForeColor = System.Drawing.Color.Black;
+            this.btnWarehouse.Location = new System.Drawing.Point(50, 182);
+            this.btnWarehouse.Name = "btnWarehouse";
+            this.btnWarehouse.Size = new System.Drawing.Size(134, 38);
+            this.btnWarehouse.TabIndex = 12;
+            this.btnWarehouse.Text = "Warehouse";
+            this.btnWarehouse.UseVisualStyleBackColor = false;
+            // 
+            // btnCategory
+            // 
+            this.btnCategory.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCategory.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategory.ForeColor = System.Drawing.Color.Black;
+            this.btnCategory.Location = new System.Drawing.Point(202, 182);
+            this.btnCategory.Name = "btnCategory";
+            this.btnCategory.Size = new System.Drawing.Size(134, 38);
+            this.btnCategory.TabIndex = 13;
+            this.btnCategory.Text = "Category";
+            this.btnCategory.UseVisualStyleBackColor = false;
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 256);
+            this.Controls.Add(this.btnCategory);
+            this.Controls.Add(this.btnWarehouse);
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.btnPurchase);
+            this.Controls.Add(this.btnRestaurant);
+            this.Controls.Add(this.btnAccount);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.lblHelloMsg);
+            this.Name = "FrmMenu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHelloMsg;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Button btnAccount;
+        private System.Windows.Forms.Button btnRestaurant;
+        private System.Windows.Forms.Button btnPurchase;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnWarehouse;
+        private System.Windows.Forms.Button btnCategory;
+    }
+}
