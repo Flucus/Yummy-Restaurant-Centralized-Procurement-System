7cca7b8186abeab39edf971f843730f32f7f9db6
Ka Wing CHAN <220014654@stu.vtc.edu.hk>
2023-05-31 13:32:09 +0800

    test by kelvin

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -57,9 +57,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(151, 199);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.Size = new System.Drawing.Size(34, 12);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "kelvin";
             // 
             // Form1
             // 
