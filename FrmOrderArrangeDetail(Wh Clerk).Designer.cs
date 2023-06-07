2c212cef0943dbcc243947a4d0aa3cdb5a7bee1c
Ka Wing CHAN <220014654@stu.vtc.edu.hk>
2023-06-08 00:39:55 +0800

    -Udpata Order Arrage

--- FrmOrderArrangeDetail(Wh Clerk).Designer.cs
+++ FrmOrderArrangeDetail(Wh Clerk).Designer.cs
@@ -29,12 +29,65 @@ namespace YummyRestaurantSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblRestaurant = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblRestaurant
+            // 
+            this.lblRestaurant.AutoSize = true;
+            this.lblRestaurant.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRestaurant.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblRestaurant.Location = new System.Drawing.Point(12, 9);
+            this.lblRestaurant.Name = "lblRestaurant";
+            this.lblRestaurant.Size = new System.Drawing.Size(424, 24);
+            this.lblRestaurant.TabIndex = 30;
+            this.lblRestaurant.Text = "The Yummy Restaurant Order Arrange Detail";
+            this.lblRestaurant.Click += new System.EventHandler(this.lblRestaurant_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(772, 313);
+            this.dataGridView1.TabIndex = 31;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.Control;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnClose.Location = new System.Drawing.Point(16, 355);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(134, 35);
+            this.btnClose.TabIndex = 33;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // FrmOrderArrangeDetail_Wh_Clerk_
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 409);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblRestaurant);
+            this.Name = "FrmOrderArrangeDetail_Wh_Clerk_";
             this.Text = "FrmOrderArrangeDetail_Wh_Clerk_";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblRestaurant;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClose;
     }
 }
--- FrmOrderArrangeDetail(Wh Clerk).cs
+++ FrmOrderArrangeDetail(Wh Clerk).cs
@@ -16,5 +16,10 @@ namespace YummyRestaurantSystem
         {
             InitializeComponent();
         }
+
+        private void lblRestaurant_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
