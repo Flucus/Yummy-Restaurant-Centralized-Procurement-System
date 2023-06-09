dccae398884a6998981b9a5115a3702fffc841e1
hospotho <a155268747@gmail.com>
2023-06-10 04:59:04 +0800

    add salt generate

--- SQLHandler.cs
+++ SQLHandler.cs
@@ -13,6 +13,14 @@ namespace YummyRestaurantSystem
     public static class SQLHandler
     {
         private static readonly string connString = "server=127.0.0.1;port=3306;user id=root;password=;database=YummyRestaurantGroupDB;charset=utf8;";
+        private static Random random = new Random();
+
+        public static string GenerateSalt()
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const int length = 16;
+            return new string(Enumerable.Range(1, length).Select(_ => chars[random.Next(chars.Length)]).ToArray());
+        }
 
         public static DataRow CheckLogin(string acc, string password)
         {
